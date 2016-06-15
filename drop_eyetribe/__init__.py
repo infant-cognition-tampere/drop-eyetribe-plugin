9fa74c73a5da549e5b22d0254e07d5cbb3705878
Mika Tammi <mikatammi@gmail.com>
2016-06-15 16:16:19 +0300

    Base for plugin.
    - Setuptools config for Python package.
    - Initial bare plugin module.
    
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

---
+++ drop_eyetribe/__init__.py
@@ -0,0 +1,20 @@
+"""This plugin provides TheEyeTribe support for drop."""
+from yapsy.IPlugin import IPlugin
+import os.path
+
+
+class EyeTribePlugin(IPlugin):
+    """Interface class for Yapsy."""
+
+    def get_sensor(self, rootdir, savedir, on_rec_cre, on_rec_err):
+        """Construct sensor."""
+        pass
+
+
+def get_plugin():
+    """Entry point function for setuptools/distutils."""
+    abspath = os.path.abspath(__file__)
+    dirname = os.path.dirname(abspath)
+    pluginpath = os.path.join(dirname, 'eyetribe.yapsy-plugin')
+
+    return pluginpath, os.path.splitext(abspath)[0]
