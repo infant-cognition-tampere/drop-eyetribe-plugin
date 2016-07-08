09886cde89e0ad52b47f203db5698cb41d8a1e91
Mika Tammi <mikatammi@gmail.com>
2016-07-08 05:16:41 +0300

    Socket thread to communicate with The Eye Tribe server.
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- drop_eyetribe/EyetrackerEyeTribe.py
+++ drop_eyetribe/EyetrackerEyeTribe.py
@@ -1,7 +1,82 @@
 """TheEyeTribe plugin for drop."""
 from drop.Sensor import Sensor
 
+from threading import Thread
+from Queue import Queue
+import socket
+from select import select
+
 import glib
+
+
+class EyeTribeSocket(Thread):
+    """Thread for socket-based communication with EyeTribe server."""
+
+    def __init__(self, host="localhost", port=6555, callback=None):
+        """Constructor."""
+        super(EyeTribeSocket, self).__init__()
+
+        # Handle messages with callback
+        self.callback = callback
+
+        # Create new non-blocking socket and connect
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
+        self.sock.setblocking(0)
+
+        self.send_queue = Queue()
+
+    def send(self, msg):
+        """
+        Put a packet into send queue.
+
+        Thread main loop will process it in sending phase.
+        """
+        # TODO MAYBE: Check message validity before sending
+        # Put message into send_queue
+        self.send_queue.put(msg)
+
+    def run(self):
+        """Main loop of the socket thread."""
+        partial_data = ""
+
+        while self.should_run:
+            # Get stuff from send_queue and send
+            while not self.send_queue.empty():
+                msg = self.send_queue.get(False)
+                self.sock.send(msg)
+                self.send_queue.task_done()
+
+            # Select from sockets
+            # TODO: Somewhat hacky solution to use 0.01 timeout,
+            #       examine the possibility to use "signals."
+            read_sockets, write_sockets, err_sockets = \
+                select([self.sock], [], [], 0.01)
+
+            for sock in read_sockets:
+                if sock == self.sock:
+                    read_data = sock.recv(512)
+                    if not read_data:
+                        raise IOError
+
+                    read_data = partial_data + read_data
+
+                    msgs = read_data.split('\n')
+                    for msg in msgs[:-1]:
+                        # Do a callback for received messages
+                        if self.callback is not None:
+                            self.callback(msg)
+
+                    partial_data = msgs[-1]
+
+    def start(self):
+        """Start the socket thread."""
+        self.should_run = True
+        return super(EyeTribeSocket, self).start()
+
+    def stop(self):
+        """Cause the socket loop to exit."""
+        self.should_run = False
 
 
 class EyeTribeET(Sensor):
