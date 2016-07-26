b4ef85c5026f46a9a6a418eb1670a3fd27e947a0
Mika Tammi <mikatammi@gmail.com>
2016-07-26 17:22:16 +0300

    JSON to Tab delimited format conversion
    - Convert eyetribe data to format recognized by other analysis tools
      used by research team.
    
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- drop_eyetribe/EyetrackerEyeTribe.py
+++ drop_eyetribe/EyetrackerEyeTribe.py
@@ -9,6 +9,141 @@ import json
 import nudged
 import glib
 import os
+import re
+from datetime import datetime
+import time
+import csv
+
+
+# Regular expression to match timestamp format given by eyetribe server.
+# Compiled initially during import of module.
+_timestamp_matcher = re.compile(
+    "^(?P<year>[0-9]{4})-(?P<month>[0-9]{2})-(?P<day>[0-9]{2}) "
+    "(?P<hour>[0-9]{2}):(?P<minute>[0-9]{2}):(?P<second>[0-9]{2})\."
+    "(?P<millisecond>[0-9]{3})$")
+
+
+def _parse_timestamp(ts):
+    m = _timestamp_matcher.match(ts)
+    dt = datetime(year=int(m.group('year')),
+                  month=int(m.group('month')),
+                  day=int(m.group('day')),
+                  hour=int(m.group('hour')),
+                  minute=int(m.group('minute')),
+                  second=int(m.group('second')),
+                  microsecond=int(
+                      m.group('millisecond')) * 1000)
+
+    return (time.mktime(dt.timetuple()) * 1000) + \
+           (dt.microsecond / 1000) / 1000.0
+
+
+def _get_validity_from_state(state):
+    if state & 0x1 and state & 0x2 and state & 0x4:
+        return 0
+    return -1
+
+
+def _convert_gazedata_frame(frame):
+    # >LeftEyeNx
+    # >LeftEyeNy
+    # >LeftEyePosition3dRelativeX
+    # >LeftEyePosition3dRelativeY
+    #  LeftEyePosition3dRelativeZ ?
+    #  LeftEyePosition3dX
+    #  LeftEyePosition3dY
+    #  LeftEyePosition3dZ
+    # >LeftEyePupilDiameter
+    # >RightEyeNx
+    # >RightEyeNy
+    # >RightEyePosition3dRelativeX
+    # >RightEyePosition3dRelativeY
+    #  RightEyePosition3dRelativeZ ?
+    #  RightEyePosition3dX ?
+    #  RightEyePosition3dY ?
+    #  RightEyePosition3dZ ?
+    # >RightEyePupilDiameter
+    # >TETTime
+    # >ValidityLeftEye
+    # >ValidityRightEye
+    # >XGazePosLeftEye
+    # >XGazePosRightEye
+    # >YGazePosLeftEye
+    # >YGazePosRightEye
+
+    row = {'XGazePosLeftEye': frame['lefteye']['raw']['x'],
+           'YGazePosLeftEye': frame['lefteye']['raw']['y'],
+           'XGazePosRightEye': frame['righteye']['raw']['x'],
+           'YGazePosRightEye': frame['righteye']['raw']['y'],
+           'LeftEyeNx': frame['lefteye_nudged']['raw']['x'],
+           'LeftEyeNy': frame['lefteye_nudged']['raw']['y'],
+           'RightEyeNx': frame['righteye_nudged']['raw']['x'],
+           'RightEyeNy': frame['righteye_nudged']['raw']['y'],
+           'LeftEyePosition3dRelativeX':
+               1.0 - frame['lefteye']['pcenter']['x'],
+           'LeftEyePosition3dRelativeY':
+               frame['lefteye']['pcenter']['y'],
+           'RightEyePosition3dRelativeX':
+               1.0 - frame['righteye']['pcenter']['x'],
+           'RightEyePosition3dRelativeY':
+               frame['righteye']['pcenter']['y'],
+           'LeftEyePupilDiameter': frame['lefteye']['psize'],
+           'RightEyePupilDiameter': frame['righteye']['psize'],
+           'ValidityBothEyes':
+               _get_validity_from_state(frame['state']),
+           'TETTime':
+               _parse_timestamp(frame['timestamp'])}
+    return row
+
+
+def _convert_json_to_tabdelim(source_filename, dest_filename):
+    """Convert file from JSON to CSV format."""
+    with open(source_filename, 'r') as json_file:
+        json_lines = json_file.readlines()
+
+    json_objects = map(json.loads, json_lines)
+
+    json_frames = filter(lambda x: 'frame' in x, json_objects)
+    json_tags = filter(lambda x: 'tag' in x, json_objects)
+
+    frame_dicts = [_convert_gazedata_frame(f['frame']) for f in json_frames]
+
+    frame_keys = list(frozenset(reduce(
+        lambda x, y: x + y, [d.keys() for d in frame_dicts])))
+
+    tag_keys = list(frozenset(reduce(
+        lambda x, y: x + y, [t['tag'].keys() for t in json_tags])))
+    tag_keys = filter(lambda x: x != 'secondary_id', tag_keys)
+
+    # Generate list of start-end tags
+    # Assuming that start and end tags always follow each other and that
+    # there are even amount of tags
+    assert len(json_tags) % 2 == 0
+
+    tags = zip(*[iter([t['tag'] for t in json_tags])]*2)
+
+    # Modify frame dicts to contain tag information where present
+    for f in frame_dicts:
+        frame_time = f['TETTime']
+        for t in tags:
+            assert t[0]['secondary_id'] == 'start' and \
+                t[1]['secondary_id'] == 'end'
+
+            start_time = t[0]['timestamp']
+            end_time = t[1]['timestamp']
+
+            if frame_time > start_time and frame_time < end_time:
+                tagdict = dict(t)
+                tagdict.pop('secondary_id')
+
+                f.update(tagdict)
+
+    with open(dest_filename, 'w') as csv_file:
+        writer = csv.DictWriter(csv_file,
+                                fieldnames=frame_keys + tag_keys,
+                                dialect='excel-tab')
+        writer.writeheader()
+        writer.writerows(frame_dicts)
 
 
 class EyeTribeSocket(Thread):
@@ -271,7 +406,7 @@ class EyeTribeET(Sensor):
             'righteye_nudged': {'raw': {'x': gaze_right_x, 'y': gaze_right_y}}
         })
         if self.collect_data:
-            self.collect_file.write(json.dumps(frame) + '\n')
+            self.collect_file.write(json.dumps({'frame': frame}) + '\n')
 
         # Calibration & linear transformation section
         if self.nudged_current_range is not None:
@@ -316,8 +451,6 @@ class EyeTribeET(Sensor):
 
     def tag(self, tag):
         """Called when tag needs to be inserted into data."""
-        print "TAG %s" % tag
-
         if self.collect_data:
             self.collect_file.write(json.dumps({'tag': tag}) + '\n')
 
@@ -384,17 +517,27 @@ class EyeTribeET(Sensor):
 
     def stop_recording(self):
         """Called when recording should be stopped."""
-        self.collect_data = False
-        self.collect_file.close()
+        if self.collect_data:
+            self.collect_data = False
+            self.collect_file.close()
+
+            _convert_json_to_tabdelim(self.collect_filename + '.json',
+                                      self.collect_filename)
 
     def start_recording(self, rootdir, participant_id, experiment_file,
                         section_id):
         """Called when recording should be started."""
-        expname = os.path.basename(experiment_file).split('.')[0]
-        fname = '%s_%s_%s.gazedata' % (expname, participant_id, section_id)
-        fname = os.path.join(rootdir, fname)
+        assert not self.collect_data
 
-        self.collect_file = open(fname, 'w')
+        expname = os.path.basename(experiment_file).split('.')[0]
+        fname = '%s_%s_%s.gazedata' % (expname,
+                                       participant_id,
+                                       section_id)
+        fname = os.path.join(rootdir, fname)
+        json_fname = fname + '.json'
+
+        self.collect_file = open(json_fname, 'w')
+        self.collect_filename = fname
 
         metadata = json.dumps({'metadata': self.tracker.values})
         self.collect_file.write(metadata + '\n')
